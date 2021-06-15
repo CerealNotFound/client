96f2b16130de30d973b9f27aa68381a20f0360e7
Harsh Rastogi <rastogiharsh04@gmail.com>
2021-06-15 18:36:42 +0530

    Config Created Eslint-React Rules Added Prettier Rules Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -7,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Project App</h1>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/javasript-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View Source
         </a>
       </header>
     </div>
