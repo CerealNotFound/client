49eb7b28047ac25eb45591c1ca375a97e355f446
Adarsh Pandey <72287529+CerealNotFound@users.noreply.github.com>
2021-07-05 21:45:59 +0530

    Add files via upload

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "../../components/Header/Header";
+import Background from "../../components/Background/Background";
+
+const LandingPage = () => {
+    return(
+        <div>
+            <Header />
+            <Background />
+        </div>
+    )
+}
+
+export default LandingPage;
