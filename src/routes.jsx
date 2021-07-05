4ee4df3ad9b3c1479097a4255c9955cb4479519e
Adarsh Pandey <72287529+CerealNotFound@users.noreply.github.com>
2021-07-05 21:48:41 +0530

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import ReactPage from './pages/Display/App';
+import LandingPage from './pages/LandingPage/LandingPage';
+// import ReactPage from './pages/Display/App';
 
 // Create Routes With Page Name
 const Routes = () => {
-  return <Route exact path="/" component={ReactPage} />;
-};
+  return (<Route exact path="/" component={LandingPage}>
+    <LandingPage />
+  </Route>);
 
 export default Routes;
